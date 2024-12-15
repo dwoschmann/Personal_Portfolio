c1f87b0086681b8f29a59337448ff20c4f5976be
Daniel Oschmann <146797627+dwoschmann@users.noreply.github.com>
2024-12-14 19:54:01 -0800

    Update CovidDataQueries.sql

--- Data_Analysis_Portfolio/CovidDataQueries.sql
+++ Data_Analysis_Portfolio/CovidDataQueries.sql
@@ -6,10 +6,10 @@ SELECT *
   FROM `COVID.CovidVaccines`
   ORDER BY 3,4;
 
-
 SELECT location, date, total_cases, new_cases, total_deaths, population
   FROM `COVID.CovidDeaths`
   ORDER BY 1,2;
+
 
 
 -- Total Cases vs Total Deaths
@@ -17,16 +17,19 @@ SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100
   FROM `COVID.CovidDeaths`
   ORDER BY 1,2;
 
+
 -- Total Cases vs Population
 SELECT location, date, total_cases, population, (total_cases/population)*100 as InfectedPercentage
   FROM `COVID.CovidDeaths`
   ORDER BY 1,2;
+
 
 -- Countries with Highest Infection Rate vs Population
 SELECT location, MAX(total_cases) as HighestInfectionCount, population, MAX((total_cases/population))*100 as InfectedPercentage
   FROM `COVID.CovidDeaths`
   GROUP BY location, population
   ORDER BY InfectedPercentage DESC;
+
 
 -- Countries with Highest Death Count vs Population
 SELECT location, MAX(total_deaths) as TotalDeathCount
@@ -41,8 +44,10 @@ SELECT location, MAX(total_deaths) as TotalDeathCount
   FROM `COVID.CovidDeaths`
   WHERE continent is null
   -- where clause to exlude groupings of countries
+  AND location not in ('World', 'European Union', 'International')
   GROUP BY location
   ORDER BY TotalDeathCount DESC;
+
 
 --Global Numbers
 SELECT SUM(new_cases) as total_cases, SUM(new_deaths) as total_deaths, SUM(new_deaths)/SUM(new_cases)*100 as DeathPercentage
@@ -60,6 +65,7 @@ SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinatio
     and dea.date = vac.date
   WHERE dea.continent IS NOT NULL
   ORDER BY 2,3;
+
 
 -- USE A CTE TO RUN CALCULATIONS WITH RUNNING TOTAL
 WITH popvsvac AS (
