23ab8f54d6577fe38c8d1901f97ab430b9f0167f
Daniel Oschmann <146797627+dwoschmann@users.noreply.github.com>
2025-01-13 14:58:36 -0800

    Update passing_yards_short_vs_long_passes.r

--- NFL_with_Python_&_R/passing_yards_short_vs_long_passes.r
+++ NFL_with_Python_&_R/passing_yards_short_vs_long_passes.r
@@ -1,3 +1,17 @@
+install.packages(c("nflreadr", "dplyr", "tidyverse", "gt", "gtExtras", "nflfastR", "ggimage", "ggthemes", "ggtext", "ggplot2", "ggrepel"))
+
+library(nflreadr)
+library(dplyr)
+library(tidyverse)
+library(gt)
+library(gtExtras)
+library(nflfastR)
+library(ggimage)
+library(ggthemes)
+library(ggtext)
+library(ggplot2)
+library(ggrepel)
+
 pbp_r <- load_pbp(2021:2024)
 
 pbp_r_p <-
@@ -24,17 +38,22 @@ pbp_r_p |>
 
 ggplot(pbp_r, aes(x = passing_yards)) +
   geom_histogram()
-  
-pbp_r_p |>
-  filter(pass_length_air_yards == "long") |>
-  ggplot(aes(passing_yards)) +
-  geom_histogram(binwidth = 1) +
-  ylab("Count")
-  xlab("Net yards on 'long' passing plays") +
-  theme_bw()
 
-ggplot(pbp_r_p, aes(x = pass_length_air_yards, y = passing_yards)) +
-  geom_boxplot() +
-  theme_bw() +
-  xlab("Pass length in yards(long >= 20, short < 20 yards)") +
-  ylab("Yards gained (or lost) during a pass play")
+pbp_r_p_hist <-   
+  pbp_r_p |>
+    filter(pass_length_air_yards == "long") |>
+    ggplot(aes(passing_yards)) +
+    geom_histogram(binwidth = 1) +
+    ylab("Count")
+    xlab("Net yards on 'long' passing plays") +
+    theme_bw()
+
+pbp_r_p_boxplot <-  
+  ggplot(pbp_r_p, aes(x = pass_length_air_yards, y = passing_yards)) +
+    geom_boxplot() +
+    theme_bw() +
+    xlab("Pass length in yards(long >= 20, short < 20 yards)") +
+    ylab("Yards gained (or lost) during a pass play")
+
+pbp_r_p_hist
+pbp_r_p_boxplot
